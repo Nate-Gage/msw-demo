82363b507894318de2c23dbf7bce052e49eeb059
Nate <nate.d.gage@gmail.com>
2020-09-06 11:03:25 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const MockServiceWorkerDemo = () => {
+
+  // const fetchData = () => {
+    
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mock Service Worker!</h1>
+      <p>Since axios does not reach a server for this demo, just return some JSON.</p>
+      <button
+        
+      >Fetch Data</button>
     </div>
   );
 }
 
-export default App;
+export default MockServiceWorkerDemo;
